643ff37980a831df171afb1a6123c15b77311503
Jon Leighton <j@jonathanleighton.com>
2012-01-10 17:36:41 +0000

    Add requires

--- lib/lolsoap.rb
+++ lib/lolsoap.rb
@@ -1,2 +1,9 @@
 module LolSoap
+  require 'lolsoap/builder'
+  require 'lolsoap/client'
+  require 'lolsoap/envelope'
+  require 'lolsoap/request'
+  require 'lolsoap/response'
+  require 'lolsoap/wsdl'
+  require 'lolsoap/wsdl_parser'
 end
