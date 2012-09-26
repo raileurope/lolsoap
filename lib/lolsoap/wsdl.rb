8f66e834b1e50e720c421fe01c9de7e2550983e9
Jon Leighton <j@jonathanleighton.com>
2012-09-26 17:49:58 +0100

    Discard the parser after initialize
    This allows us to free the memory, and is conceptually cleaner. The WSDL
    instance shouldn't have to care about parsing once initialized.

--- lib/lolsoap/wsdl.rb
+++ lib/lolsoap/wsdl.rb
@@ -13,69 +13,64 @@ module LolSoap
       new(WSDLParser.parse(raw))
     end
 
-    attr_reader :parser
+    # The SOAP endpoint URL
+    attr_reader :endpoint
+
+    # Hash of namespaces used in the WSDL document (keys are prefixes)
+    attr_reader :namespaces
+
+    # Hash of namespace prefixes used in the WSDL document (keys are namespace URIs)
+    attr_reader :prefixes
+
+    # The version of SOAP detected.
+    attr_reader :soap_version
 
     def initialize(parser)
-      @parser = parser
+      @types        = load_types(parser)
+      @operations   = load_operations(parser)
+      @endpoint     = parser.endpoint
+      @namespaces   = parser.namespaces
+      @prefixes     = parser.prefixes
+      @soap_version = parser.soap_version
     end
 
     # Hash of operations that are supports by the SOAP service
     def operations
-      load_operations.dup
+      @operations.dup
     end
 
     # Get a single operation
     def operation(name)
-      load_operations.fetch(name)
+      @operations.fetch(name)
     end
 
     # Hash of types declared by the service
     def types
-      load_types.dup
+      @types.dup
     end
 
     # Get a single type, or a NullType if the type doesn't exist
     def type(name)
-      load_types.fetch(name) { NullType.new }
-    end
-
-    # The SOAP endpoint URL
-    def endpoint
-      parser.endpoint
-    end
-
-    # Hash of namespaces used in the WSDL document (keys are prefixes)
-    def namespaces
-      parser.namespaces
-    end
-
-    # Hash of namespace prefixes used in the WSDL document (keys are namespace URIs)
-    def prefixes
-      parser.prefixes
+      @types.fetch(name) { NullType.new }
     end
 
     # Namespaces used by the types (a subset of #namespaces)
     def type_namespaces
-      Hash[load_types.values.map { |type| [type.prefix, namespaces[type.prefix]] }]
-    end
-
-    # The version of SOAP detected.
-    def soap_version
-      parser.soap_version
+      Hash[@types.values.map { |type| [type.prefix, namespaces[type.prefix]] }]
     end
 
     def inspect
       "<#{self.class} " \
-      "namespaces=#{namespaces.inspect} " \
-      "operations=#{operations.keys.inspect} " \
-      "types=#{types.keys.inspect}>"
+      "namespaces=#{@namespaces.inspect} " \
+      "operations=#{@operations.keys.inspect} " \
+      "types=#{@types.keys.inspect}>"
     end
 
     private
 
     # @private
-    def load_operations
-      @operations ||= Hash[
+    def load_operations(parser)
+      Hash[
         parser.operations.map do |k, op|
           [k, Operation.new(self, op[:action], type(op[:input]), type(op[:output]))]
         end
@@ -83,8 +78,8 @@ module LolSoap
     end
 
     # @private
-    def load_types
-      @types ||= Hash[
+    def load_types(parser)
+      Hash[
         parser.types.map do |prefixed_name, type|
           [prefixed_name, Type.new(type[:name], type[:prefix], build_elements(type[:elements]))]
         end
--- test/unit/test_wsdl.rb
+++ test/unit/test_wsdl.rb
@@ -5,61 +5,18 @@ module LolSoap
   describe WSDL do
     describe 'with a doc that can be parsed' do
       let(:namespace) { 'http://lolsoap.api/bla' }
-      let(:parser) { OpenStruct.new(:namespaces => { 'bla' => namespace }) }
 
-      subject { WSDL.new(parser) }
-
-      describe 'with operations' do
-        before do
-          def subject.type(n)
-            @types ||= { 'WashHandsRequest' => Object.new, 'WashHandsResponse' => Object.new }
-            @types[n]
-          end
-
-          parser.operations = {
+      let(:parser) do
+        OpenStruct.new(
+          :namespaces => { 'bla' => namespace },
+          :operations => {
             'washHands' => {
               :action => 'urn:washHands',
-              :input  => 'WashHandsRequest',
-              :output => 'WashHandsResponse'
+              :input  => 'bla:Brush',
+              :output => 'bla:Color'
             }
-          }
-        end
-
-        describe '#operations' do
-          it 'returns a hash of operations' do
-            subject.operations.length.must_equal 1
-            subject.operations['washHands'].tap do |op|
-              op.wsdl.must_equal   subject
-              op.action.must_equal "urn:washHands"
-              op.input.must_equal  subject.types['WashHandsRequest']
-              op.output.must_equal subject.types['WashHandsResponse']
-            end
-          end
-        end
-
-        describe '#operation' do
-          it 'returns a single operation' do
-            subject.operation('washHands').must_equal subject.operations['washHands']
-          end
-        end
-      end
-
-      describe '#endpoint' do
-        it 'returns the endpoint' do
-          parser.endpoint = 'http://lolsoap.api/v1'
-          subject.endpoint.must_equal 'http://lolsoap.api/v1'
-        end
-      end
-
-      describe '#namespaces' do
-        it 'returns a namespaces hash' do
-          subject.namespaces.must_equal({ 'bla' => namespace })
-        end
-      end
-
-      describe 'with types' do
-        before do
-          parser.types = {
+          },
+          :types => {
             'bla:Brush' => {
               :elements => {
                 'handleColor' => {
@@ -87,43 +44,76 @@ module LolSoap
               :prefix => 'bla'
             }
           }
-        end
+        )
+      end
 
-        describe '#types' do
-          it 'returns a hash of types' do
-            subject.types.length.must_equal 2
+      subject { WSDL.new(parser) }
 
-            subject.types['bla:Brush'].tap do |t|
-              t.elements.length.must_equal 2
-              t.element('handleColor').type.must_equal subject.types['bla:Color']
-              t.element('handleColor').singular?.must_equal true
-              t.element('age').type.must_equal WSDL::NullType.new
-              t.element('age').singular?.must_equal false
-            end
-
-            subject.types['bla:Color'].tap do |t|
-              t.elements.length.must_equal 2
-              t.element('name').type.must_equal WSDL::NullType.new
-              t.element('hex').type.must_equal WSDL::NullType.new
-            end
+      describe '#operations' do
+        it 'returns a hash of operations' do
+          subject.operations.length.must_equal 1
+          subject.operations['washHands'].tap do |op|
+            op.wsdl.must_equal   subject
+            op.action.must_equal "urn:washHands"
+            op.input.must_equal  subject.types['bla:Brush']
+            op.output.must_equal subject.types['bla:Color']
           end
         end
+      end
 
-        describe '#type' do
-          it 'returns a single type' do
-            subject.type('bla:Color').must_equal subject.types['bla:Color']
+      describe '#operation' do
+        it 'returns a single operation' do
+          subject.operation('washHands').must_equal subject.operations['washHands']
+        end
+      end
+
+      describe '#endpoint' do
+        it 'returns the endpoint' do
+          parser.endpoint = 'http://lolsoap.api/v1'
+          subject.endpoint.must_equal 'http://lolsoap.api/v1'
+        end
+      end
+
+      describe '#namespaces' do
+        it 'returns a namespaces hash' do
+          subject.namespaces.must_equal({ 'bla' => namespace })
+        end
+      end
+
+      describe '#types' do
+        it 'returns a hash of types' do
+          subject.types.length.must_equal 2
+
+          subject.types['bla:Brush'].tap do |t|
+            t.elements.length.must_equal 2
+            t.element('handleColor').type.must_equal subject.types['bla:Color']
+            t.element('handleColor').singular?.must_equal true
+            t.element('age').type.must_equal WSDL::NullType.new
+            t.element('age').singular?.must_equal false
           end
 
-          it 'returns a null object if a type is missing' do
-            subject.type('FooBar').must_equal WSDL::NullType.new
+          subject.types['bla:Color'].tap do |t|
+            t.elements.length.must_equal 2
+            t.element('name').type.must_equal WSDL::NullType.new
+            t.element('hex').type.must_equal WSDL::NullType.new
           end
         end
+      end
 
-        describe '#type_namespaces' do
-          it 'returns only the namespaces that are used by types' do
-            parser.namespaces['foo'] = 'bar'
-            subject.type_namespaces.must_equal 'bla' => namespace
-          end
+      describe '#type' do
+        it 'returns a single type' do
+          subject.type('bla:Color').must_equal subject.types['bla:Color']
+        end
+
+        it 'returns a null object if a type is missing' do
+          subject.type('FooBar').must_equal WSDL::NullType.new
+        end
+      end
+
+      describe '#type_namespaces' do
+        it 'returns only the namespaces that are used by types' do
+          parser.namespaces['foo'] = 'bar'
+          subject.type_namespaces.must_equal 'bla' => namespace
         end
       end
     end
