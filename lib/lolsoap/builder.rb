3815751f68bc0da20cdf29370b2c09266c1ddb96
sami <sami@effilab-local.com>
2017-07-19 16:30:49 +0200

    add comment remove useless code

--- lib/lolsoap/builder.rb
+++ lib/lolsoap/builder.rb
@@ -3,17 +3,14 @@ require 'lolsoap/builder/hash_params'
 require 'lolsoap/builder/block_params'
 
 module LolSoap
-  # Decorator
+  # Instanciate the class adapted to params
   class Builder < SimpleDelegator
-    def initialize(node, type = WSDL::NullType.new)
-      type ||= WSDL::NullType.new
-
+    def initialize(node, type)
       if block_given?
         yield super(BlockParams.new(node, type))
       else
         super(HashParams.new(node, type))
       end
     end
-
   end
 end
