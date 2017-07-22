02d836f2aa9bbaa44b581a758d4a06de8de71aff
sami <sami@effilab-local.com>
2017-07-22 12:04:42 +0200

    Suppress warnings with better object management Add tests

--- test/unit/test_callbacks.rb
+++ test/unit/test_callbacks.rb
@@ -1,14 +1,17 @@
 require 'helper'
 require 'lolsoap/callbacks.rb'
-require 'byebug'
 
 module LolSoap
   describe Callbacks do
     before do
-      load 'lolsoap/callbacks.rb'
       @lol_callbacks = Callbacks.new.tap do |lc|
         lc.for('a.b') << ->(name, mutable) { mutable << "Lol #{name}" }
       end
+    end
+
+    after do
+      @lol_callbacks.disable
+      @lol_callbacks = nil
     end
 
     it 'can store one callback' do
@@ -25,10 +28,20 @@ module LolSoap
       ary.must_equal ['Lol lol']
     end
 
+    it 'can call multiple callbacks' do
+      temp = Callbacks.new
+      temp.for('a.b') << ->(name, mutable) { mutable << "#{name} more" }
+      temp.for('a.b') << ->(name, mutable) { mutable << "#{name} again" }
+      ary = []
+      Callbacks.in('a.b').expose('lol', ary)
+      ary.must_equal ['Lol lol', 'lol more', 'lol again']
+      temp.disable
+      temp = nil
+    end
+
     it 'can be disabled' do
       @lol_callbacks.disable
       Callbacks.instance_variable_get(:@registered).size.must_equal 0
-      @lol_callbacks.enable
     end
 
     it 'can be enabled' do
