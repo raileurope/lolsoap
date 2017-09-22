a95b5958aeab8a6d11b5591056e14d1b5f7f4955
sami <sami@effilab-local.com>
2017-09-22 11:33:52 +0200

    Add callbacks tests.

--- test/unit/test_callbacks.rb
+++ test/unit/test_callbacks.rb
@@ -1,5 +1,6 @@
 require 'helper'
 require 'lolsoap/callbacks.rb'
+require 'byebug'
 
 module LolSoap
   describe Callbacks do
@@ -44,6 +45,47 @@ module LolSoap
       temp.disable
     end
 
+    it 'can keeep the order on clear' do
+      temp = Callbacks.new
+      temp.for('a.b') << ->(name, mutable) { mutable << "#{name} more" }
+      ary = []
+      Callbacks.in('a.b').expose('lol', ary)
+      ary.must_equal ['Lol lol', 'lol more']
+      @lol_callbacks.for('a.b').clear
+      ary = []
+      Callbacks.in('a.b').expose('lol', ary)
+      ary.must_equal ['lol more']
+      @lol_callbacks.for('a.b') << ->(name, mutable) { mutable << "Lol #{name}" }
+      ary = []
+      Callbacks.in('a.b').expose('lol', ary)
+      ary.must_equal ['Lol lol', 'lol more']
+      temp.disable
+    end
+
+    it 'can keeep the order on clear in a thread' do
+      Thread.new do
+        lol_callbacks = Callbacks.new.tap do |lc|
+          lc.for('a.b') << ->(name, mutable) { mutable << "Lol #{name}" }
+        end
+
+        temp = Callbacks.new
+        temp.for('a.b') << ->(name, mutable) { mutable << "#{name} more" }
+        ary = []
+        Callbacks.in('a.b').expose('lol', ary)
+        ary.must_equal ['Lol lol', 'lol more']
+        lol_callbacks.for('a.b').clear
+        ary = []
+        Callbacks.in('a.b').expose('lol', ary)
+        ary.must_equal ['lol more']
+        lol_callbacks.for('a.b') << ->(name, mutable) { mutable << "Lol #{name}" }
+        ary = []
+        Callbacks.in('a.b').expose('lol', ary)
+        ary.must_equal ['Lol lol', 'lol more']
+        lol_callbacks.disable
+        temp.disable
+      end.join
+    end
+
     it 'can route callbacks' do
       temp = Callbacks.new
       temp.for('c.d') << ->(name, mutable) { mutable << "#{name} c.D" }
@@ -63,6 +105,17 @@ module LolSoap
       ary = []
       Callbacks.in('a.b').expose('lol', ary)
       ary.must_equal ['any lol ?']
+      temp.disable
+    end
+
+    it "can't keep the order on enable" do
+      @lol_callbacks.disable
+      temp = Callbacks.new
+      temp.for('a.b') << ->(name, mutable) { mutable << "any #{name} ?" }
+      ary = []
+      @lol_callbacks.enable
+      Callbacks.in('a.b').expose('lol', ary)
+      ary.must_equal ['any lol ?', 'Lol lol']
       temp.disable
     end
 
