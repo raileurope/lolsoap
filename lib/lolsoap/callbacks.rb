22e0de010e251e0813103440640e9db02f4baf27
Damian Le Nouaille <dam@dln.name>
2017-10-20 17:24:23 +0200

    documentation updated

--- lib/lolsoap/callbacks.rb
+++ lib/lolsoap/callbacks.rb
@@ -1,21 +1,20 @@
 # Used to add user processing in defined hooks.
 #
 # @example General
-#  bing_ads_callbacks = LolSoap::Callbacks.new
-#  bing_ads_callbacks.for('hash_params.before_build') << lambda do |args, node, type|
-#    # I want to use snake case !
-#    matcher = type.elements.keys.map { |name| name.tr('_', '').downcase }
-#    args.each do |h|
-#      found_at = matcher.index(h[:name].tr('_', '').downcase)
-#      h[:name] = type.elements.keys[found_at] if found_at
-#    end
-#    # This API accepts the nodes only in the right order.
-#    args.sort_by! { |h| type.elements.keys.index(h[:name]) || 1 / 0.0 }
-#  end
+#   LolSoap::Callbacks.register({'hash_params.before_build' => [->(args, node, type) {
+#       # I want to use snake case !
+#       matcher = type.elements.keys.map { |name| name.tr('_', '').downcase }
+#       args.each do |h|
+#         found_at = matcher.index(h[:name].tr('_', '').downcase)
+#         h[:name] = type.elements.keys[found_at] if found_at
+#       end
+#       # This API accepts the nodes only in the right order.
+#       args.sort_by! { |h| type.elements.keys.index(h[:name]) || 1 / 0.0 }
+#     }
+#   ]})
 #
-# @example Managing callback sets
-# bing_ads_callbacks.disable
-# google_ads_callbacks.enable
+# @example Flush all callbacks
+#   LolSoap::Callbacks.flush_callbacks
 #
 class LolSoap::Callbacks
   # Aggregates all callbacks on the selected key to call them.
